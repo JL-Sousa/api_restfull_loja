177bcecd2a396c5f6f7bd29c12a7243c39e3f61a
Jonyelson Lopes <jonyelson20@gmail.com>
2022-12-21 16:12:43 -0300

    feat: UserController and UserRepository

---
+++ src/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+
+@Controller('/users')
+export class UserController {
+  private userRepository = new UserRepository();
+  @Post()
+  async createUser(@Body() userData) {
+    this.userRepository.save(userData);
+    return userData;
+  }
+}
